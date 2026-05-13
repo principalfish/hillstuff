6415f48d84933bf87e1f494bc77ea07062f6b78a
principalfish <principalfish@gmail.com>
2026-05-13 23:12:14 +0100

    gpx

--- misc-scripts/serve.py
+++ misc-scripts/serve.py
@@ -29,6 +29,16 @@ class Handler(SimpleHTTPRequestHandler):
             self.send_header("Content-Length", str(len(body)))
             self.end_headers()
             self.wfile.write(body)
+        elif self.path == "/api/gpx":
+            gpx = sorted(
+                os.path.basename(f) for f in glob.glob(os.path.join(SCRIPT_DIR, "*.gpx"))
+            )
+            body = json.dumps(gpx).encode()
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.send_header("Content-Length", str(len(body)))
+            self.end_headers()
+            self.wfile.write(body)
         else:
             super().do_GET()
 
